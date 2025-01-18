71fe678448cd1af46b35b86bff3986952824304a
sunwoopark27 <76925408+sunwoopark27@users.noreply.github.com>
2025-01-18 18:17:51 +0900

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -4,7 +4,7 @@
 # - DROP TABLE or DATABASE : 테이블 이나 데이터베이스 삭제
 #                            FOREIGN KEY 설정되어 있는 경우, 해당 조건 해제 후 삭제 가능
 # - TRUNCATE TABLE : 테이블을 삭제한 후 재 생성
-#                    DELETE 와 유사하지만 Table을 DROP후 재생성하기 때문에 ID(PK)값이 초기화됨
+#                    DROP 와 유사하지만 Table을 DROP후 재생성하기 때문에 ID(PK)값이 초기화됨
 
 use db;
 ## CREATE
@@ -79,7 +79,7 @@ DROP TABLE new_examples;
 
 ## TRUNCATE
 # <문법>
-# TRUNCATE [TABLE] table_name;
+# TRUNCATE TABLE table_name;
 
 # ++ SHOW
 # - DB 목록 확인
