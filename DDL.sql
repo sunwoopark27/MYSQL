6e4aaf593da101e68b96412129a7dfcca5cd26d2
sunwoopark27 <76925408+sunwoopark27@users.noreply.github.com>
2025-01-18 16:19:03 +0900

    Create DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,8 @@
+# 데이터 베이스 DDL 
+# - CREATE TABLE : 테이블 생성
+# - ALTER TABLE  : 테이블 수정
+# - DROP TABLE or DATABASE : 테이블 이나 데이터베이스 삭제
+# - TRUNCATE TABLE : 테이블을 삭제한 후 재 생성
+#                    Table을 DROP후 재생성하기 때문에 ID(PK)값이 초기화됨
+
+#
