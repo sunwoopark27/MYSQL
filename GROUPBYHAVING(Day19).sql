bdcce34954a4c1bb35ee169e22930763b9382677
sunwoopark27 <76925408+sunwoopark27@users.noreply.github.com>
2025-01-19 09:43:31 +0900

    Update GROUPBYHAVING(Day19).sql

--- GROUPBYHAVING(Day19).sql
+++ GROUPBYHAVING(Day19).sql
@@ -1,27 +1,40 @@
-use world;
-# MySQL 에서 제공하는 world db 사용
+-- SELECT SORTING --
+# : 데이터를 정렬하여 조회
 
-# country 테이블에서
-# GovernmentForm 필드의 데이터를 내림차순으로 정렬하여 조회
+-- 1. ORDER BY : 조회 결과의 레코드 <정렬>
+# < 문법 >
+# SELECT
+# select_list
+# FROM
+# table_name
+# ORDER BY
+# column1 [ASC | DESC],
+# column2 [ASC | DESC],...;
+
+# 하나 이상의 칼럼을 기준으로 결과를 오름차순(ASC, 기본값), 내림차순(DESC)으로 정렬
+# 정렬에서 NULL값이 존재할 경우, 오름차순 정렬시 결과에 NULL이 먼저 출력
+
+# world db 사용
+USE world;
+
+# 1-1. country 테이블에서 GovernmentForm 필드의 데이터를 내림차순으로 정렬하여 조회
 SELECT 
     GovernmentForm
 FROM 
     country
 ORDER BY GovernmentForm DESC;
 
-# country 테이블에서
-# GovernmentForm 필드 기준으로 내림차순으로 정렬한 후
-# SurfaceArea 필드 기준으로 오름차순 정렬하여 조회
-SELECT
-    GovernmentForm, SurfaceArea
-FROM
-    country
-ORDER BY GovernmentForm DESC, SurfaceArea;
+# 1-2. country 테이블에서 GovernmentForm 필드 기준으로 내림차순으로 정렬한 후
+#      SurfaceArea 필드 기준으로 오름차순 정렬하여 조회
+SELECT 
+	GovernmentForm, SurfaceArea
+From 
+	country
+ORDER BY 
+	GovernmentForm DESC, SurfaceArea ASC;
 
-# country 테이블에서
-# Continent 필드가 'Africa'인 데이터 중에
-# Population 기준으로 오름차순 정렬한 후
-# Name, Population 데이터 조회
+# 1-3. country 테이블에서 Continent 필드가 'Africa'인 데이터 중에 Population 기준으로
+#      오름차순 정렬한 후 Name, Population 데이터
 SELECT 
     Name, Population
 FROM 
@@ -30,15 +43,46 @@ WHERE
     Continent = 'Africa'
 ORDER BY Population;
 
-##LIMIT
-# country 테이블에서
-# Name, SurfaceArea, Population 데이터를
-# Population 내림차순 기준으로 6번째부터 12번째 데이터만 조회
+# 1-4. country 테이블에서 Continent 필드가 'Africa'인 데이터 중에
+#      IndepYear 기준으로 오름차순 정렬할때 NULL 데이터를 마지막에 위치하게 Name, IndepYear 데이터 조회
+SELECT
+	Name, IndepYear
+FROM
+	country
+WHERE
+	Continent = 'Africa'
+ORDER BY
+	IndepYear IS NULL, IndepYear;
+    
+-- 2. LIMIT : 조회하는 레코드의 수를 제한
+# < 문법 >
+# SELECT
+# select_list
+# FROM
+# table_name
+# LIMIT
+# [offset,] row_count;
+
+# offset은 시작하는 레코드의 위치, 작성하지 않을경우 첫번째레코드부터 반환
+# row_count : 조회하는 최대 레코드 수를 지정
+
+# 2-1. country 테이블에서 Name, SurfaceArea, Population 데이터를
+#      Population 내림차순 기준으로 top 5 조회
+SELECT
+	Name, SurfaceArea, Population
+FROM 
+	country
+ORDER BY 
+	Population DESC
+LIMIT 5; 
+
+# 2-2. country 테이블에서 Name, SurfaceArea, Population 데이터를
+#      Population 내림차순 기준으로 6번째부터 12번째 데이터만 조회
 SELECT Name, SurfaceArea, Population
 FROM country
 ORDER BY Population DESC
 LIMIT 5, 7;
-#LIMIT 7, OFFSET 5;
+# LIMIT 7, OFFSET 5;
 # 6으로 포함해야하기 떄문에 5/ 출력할 행의 갯수 7
 
 ## Grouping
